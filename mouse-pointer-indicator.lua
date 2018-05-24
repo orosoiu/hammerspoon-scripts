28501e0d45bad96cdaddec17e79be3bbe905a6b7
Ovidiu Rosoiu <ovidiu.rosoiu@gmail.com>
2018-05-24 21:29:49 +0300

    Reformat code

--- mouse-pointer-indicator.lua
+++ mouse-pointer-indicator.lua
@@ -2,30 +2,28 @@
 -- when holding a modifier key while clicking the left mouse button
 
 -- config section
-local mousePointerIndicator_noOfCircles = 6  -- recommended to keep this below 10
+local mousePointerIndicator_noOfCircles = 6 -- recommended to keep this below 10
 local mousePointerIndicator_strokeColor_red = 192
 local mousePointerIndicator_strokeColor_green = 41
 local mousePointerIndicator_strokeColor_blue = 66
-local mousePointerIndicator_strokeColor_alpha = .8     -- between 0 and 1
+local mousePointerIndicator_strokeColor_alpha = .8 -- between 0 and 1
 local mousePointerIndicator_filled = false
 local mousePointerIndicator_fillColor_red = 236
 local mousePointerIndicator_fillColor_green = 208
 local mousePointerIndicator_fillColor_blue = 120
-local mousePointerIndicator_fillColor_alpha = .2     -- between 0 and 1
+local mousePointerIndicator_fillColor_alpha = .2 -- between 0 and 1
 
 function mousePointerIndicator()
     local mousepoint = hs.mouse.getAbsolutePosition()
     local circles = {}
 
     for circleNo = 1, mousePointerIndicator_noOfCircles do
-        hs.timer.doAfter(.06 * circleNo, function() 
+        hs.timer.doAfter(.06 * circleNo, function()
             local offset = 10 + (circleNo * circleNo * 2)
             local coordX = mousepoint.x - (offset / 2)
             local coordY = mousepoint.y - (offset / 2)
 
-            circles["mouseCircle"..circleNo] = hs.drawing.circle(
-                hs.geometry.rect(coordX, coordY, offset, offset)
-            )
+            circles["mouseCircle" .. circleNo] = hs.drawing.circle(hs.geometry.rect(coordX, coordY, offset, offset))
             circles["mouseCircle" .. circleNo]:setStrokeColor({
                 ["red"] = mousePointerIndicator_strokeColor_red / 255,
                 ["blue"] = mousePointerIndicator_strokeColor_blue / 255,
@@ -46,20 +44,17 @@ function mousePointerIndicator()
 
             circles["mouseCircle" .. circleNo]:show()
 
-            hs.timer.doAfter(.18, function() 
-                circles["mouseCircle" .. circleNo]:delete() 
-                circles["mouseCircle" .. circleNo] = nil 
+            hs.timer.doAfter(.18, function()
+                circles["mouseCircle" .. circleNo]:delete()
+                circles["mouseCircle" .. circleNo] = nil
             end)
         end)
     end
-
 end
 
-eventtapLeftMouseDown = hs.eventtap.new(
-                            { hs.eventtap.event.types.leftMouseDown }, 
-                            function(_)
-                                mousePointerIndicator()
-                                return false
-                            end
-                        )
+eventtapLeftMouseDown = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown },
+    function(_)
+        mousePointerIndicator()
+        return false
+    end)
 eventtapLeftMouseDown:start()
