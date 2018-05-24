a3c9093cc5e1bb26776d91651ce8a3db28f53969
Ovidiu Rosoiu <ovidiu.rosoiu@gmail.com>
2018-05-24 20:53:43 +0300

    Beautified mouse indicator code

--- mouse-pointer-indicator.lua
+++ mouse-pointer-indicator.lua
@@ -2,51 +2,64 @@
 -- when holding a modifier key while clicking the left mouse button
 
 -- config section
-mousePointerIndicator_noOfCircles = 6  -- recommended to keep this below 10
-mousePointerIndicator_circleColor_red = 192
-mousePointerIndicator_circleColor_green = 41
-mousePointerIndicator_circleColor_blue = 66
-mousePointerIndicator_circleColor_alpha = .8     -- between 0 and 1
-mousePointerIndicator_circleColor_filled = false
-mousePointerIndicator_circleColor_fill_red = 236
-mousePointerIndicator_circleColor_fill_green = 208
-mousePointerIndicator_circleColor_fill_blue = 120
-mousePointerIndicator_circleColor_fill_alpha = .2     -- between 0 and 1
+local mousePointerIndicator_noOfCircles = 6  -- recommended to keep this below 10
+local mousePointerIndicator_strokeColor_red = 192
+local mousePointerIndicator_strokeColor_green = 41
+local mousePointerIndicator_strokeColor_blue = 66
+local mousePointerIndicator_strokeColor_alpha = .8     -- between 0 and 1
+local mousePointerIndicator_filled = false
+local mousePointerIndicator_fillColor_red = 236
+local mousePointerIndicator_fillColor_green = 208
+local mousePointerIndicator_fillColor_blue = 120
+local mousePointerIndicator_fillColor_alpha = .2     -- between 0 and 1
 
 function mousePointerIndicator()
-    local filled = false
-    -- Get the current co-ordinates of the mouse pointer
-    mousepoint = hs.mouse.getAbsolutePosition()
+    local mousepoint = hs.mouse.getAbsolutePosition()
     local circles = {}
-    for circleNo = 1,mousePointerIndicator_noOfCircles do
-        hs.timer.doAfter(.06*circleNo, function() 
-            local offset = 10+circleNo*circleNo*2
-            circles["mouseCircle" .. circleNo] = hs.drawing.circle(hs.geometry.rect(mousepoint.x-offset/2, mousepoint.y-offset/2, offset, offset))
+
+    for circleNo = 1, mousePointerIndicator_noOfCircles do
+        hs.timer.doAfter(.06 * circleNo, function() 
+            local offset = 10 + (circleNo * circleNo * 2)
+            local coordX = mousepoint.x - (offset / 2)
+            local coordY = mousepoint.y - (offset / 2)
+
+            circles["mouseCircle"..circleNo] = hs.drawing.circle(
+                hs.geometry.rect(coordX, coordY, offset, offset)
+            )
             circles["mouseCircle" .. circleNo]:setStrokeColor({
-                ["red"] = mousePointerIndicator_circleColor_red/255,
-                ["blue"] = mousePointerIndicator_circleColor_blue/255,
-                ["green"] = mousePointerIndicator_circleColor_green/255,
-                ["alpha"] = mousePointerIndicator_circleColor_alpha
+                ["red"] = mousePointerIndicator_strokeColor_red / 255,
+                ["blue"] = mousePointerIndicator_strokeColor_blue / 255,
+                ["green"] = mousePointerIndicator_strokeColor_green / 255,
+                ["alpha"] = mousePointerIndicator_strokeColor_alpha
             })
-            circles["mouseCircle" .. circleNo]:setFill(mousePointerIndicator_circleColor_filled)
-            circles["mouseCircle" .. circleNo]:setFillColor({
-                ["red"] = mousePointerIndicator_circleColor_fill_red/255,
-                ["blue"] = mousePointerIndicator_circleColor_fill_blue/255,
-                ["green"] = mousePointerIndicator_circleColor_fill_green/255,
-                ["alpha"] = mousePointerIndicator_circleColor_fill_alpha
-            })
-            circles["mouseCircle" .. circleNo]:setStrokeWidth(circleNo/2)
+            circles["mouseCircle" .. circleNo]:setStrokeWidth(circleNo / 2)
+
+            circles["mouseCircle" .. circleNo]:setFill(mousePointerIndicator_filled)
+            if mousePointerIndicator_filled then
+                circles["mouseCircle" .. circleNo]:setFillColor({
+                    ["red"] = mousePointerIndicator_fillColor_red / 255,
+                    ["blue"] = mousePointerIndicator_fillColor_blue / 255,
+                    ["green"] = mousePointerIndicator_fillColor_green / 255,
+                    ["alpha"] = mousePointerIndicator_fillColor_alpha
+                })
+            end
+
             circles["mouseCircle" .. circleNo]:show()
+
             hs.timer.doAfter(.18, function() 
                 circles["mouseCircle" .. circleNo]:delete() 
                 circles["mouseCircle" .. circleNo] = nil 
             end)
         end)
     end
+
 end
 
-eventtapLeftMouseDown = hs.eventtap.new({ hs.eventtap.event.types.leftMouseDown }, function(event)
-    mousePointerIndicator()
-    return false
-end)
+eventtapLeftMouseDown = hs.eventtap.new(
+                            { hs.eventtap.event.types.leftMouseDown }, 
+                            function(event)
+                                mousePointerIndicator()
+                                return false
+                            end
+                        )
 eventtapLeftMouseDown:start()
